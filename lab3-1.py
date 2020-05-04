3f75f4e4c29a9fdcc78fdfb8b9c620a514a7e7c5
unknown <t.johnson57@students.clark.edugit config --global user.email t.johnson57@students.clark.edu>
2020-05-04 12:11:58 -0700

    lab 3-1

---
+++ lab3-1.py
@@ -0,0 +1,98 @@
+"""
+CTEC 121
+<your name>
+<assignment/lab name>
+<assignment/lab description
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    '''
+    print("\nBoolean literals")
+    print(True)
+    print(False)
+    print(type(True))
+    print()
+
+    print("Relational operators")
+    print("3<5",3<5)
+    print("3<=3", 3<=3)
+    print("3==3", 3==3)
+    print("3>=5", 3>=5)
+    print()
+
+
+    x=3
+    y=5
+    print("Using variables")
+    print("x:",x,"y:", y)
+    print('x<y',x<y)
+    print("x!=y", x!=y)
+    print()
+
+    print("x:",x,"y:", y)
+    if x<y:
+        print("x<y")
+    if x>y:
+        print("x>y")
+
+    print("end example")
+    print()
+
+    print("if/else statement")
+    print("x:",x,"y:", y)
+    if x < y:
+        print("x < y")
+    else:
+        print("x>=y")    
+    x = 6
+    print("x:",x,"y:", y)
+    if x < y:
+        print("x < y")
+    else:
+        print("x >= y")
+    print("end if else statement")
+    print()
+
+    #excercize 
+
+    
+    for i in range(1,11,1):
+        print(i)
+        if i%2 == 0:
+            print('Even')
+        else:
+            print('Odd')
+    print()
+
+    #alphabatize names 
+    name = "Bill"
+    firstletter = "B"
+
+    print("multi-way if example")
+    if firstletter == "A":
+        print(name)
+
+    elif firstletter == "B":
+        print(name)
+
+    elif firstletter == "C":
+        print(name)
+
+    #...
+    elif firstletter == "Y":
+        print(name)
+    
+    else:
+        print(name)      #default case name starts with Z
+    print()
+'''
+print(4/0)
+
+
+main()    
